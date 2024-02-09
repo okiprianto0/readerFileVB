def2cf5f40f962e7f0172e8f01ae1c3ad9896f1a
Oki Prianto <49381746+ZW-Rays@users.noreply.github.com>
2024-02-10 01:24:49 +0700

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Browse = New Button()
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        Reset = New Button()
+        Close = New Button()
+        filepathBtn = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        DataGridView1 = New DataGridView()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Browse
+        ' 
+        Browse.Location = New Point(190, 318)
+        Browse.Name = "Browse"
+        Browse.Size = New Size(75, 23)
+        Browse.TabIndex = 0
+        Browse.Text = "Browse"
+        Browse.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(190, 47)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 1
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Location = New Point(190, 75)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(100, 50)
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Reset
+        ' 
+        Reset.Location = New Point(316, 318)
+        Reset.Name = "Reset"
+        Reset.Size = New Size(75, 23)
+        Reset.TabIndex = 4
+        Reset.Text = "Reset"
+        Reset.UseVisualStyleBackColor = True
+        ' 
+        ' Close
+        ' 
+        Close.Location = New Point(447, 318)
+        Close.Name = "Close"
+        Close.Size = New Size(75, 23)
+        Close.TabIndex = 5
+        Close.Text = "Close"
+        Close.UseMnemonic = False
+        Close.UseVisualStyleBackColor = True
+        ' 
+        ' filepathBtn
+        ' 
+        filepathBtn.AutoSize = True
+        filepathBtn.Location = New Point(128, 47)
+        filepathBtn.Name = "filepathBtn"
+        filepathBtn.Size = New Size(49, 15)
+        filepathBtn.TabIndex = 6
+        filepathBtn.Text = "FilePath"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(128, 75)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(49, 15)
+        Label2.TabIndex = 7
+        Label2.Text = "Gambar"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(128, 133)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(30, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Json"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(190, 135)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowTemplate.Height = 25
+        DataGridView1.Size = New Size(240, 150)
+        DataGridView1.TabIndex = 9
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(750, 421)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(filepathBtn)
+        Controls.Add(Close)
+        Controls.Add(Reset)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label1)
+        Controls.Add(Browse)
+        Name = "Form1"
+        Text = "UAS Pemograman Visual - Oki Prianto"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Browse As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Reset As Button
+    Friend WithEvents Close As Button
+    Friend WithEvents filepathBtn As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    Dim filePath As String
+
+    Private Sub Browse_Click(sender As Object, e As EventArgs) Handles Browse.Click
+        'selalu di clear
+        Clear()
+
+        Dim openFileDialog As New OpenFileDialog()
+
+        openFileDialog.Filter = "All Files|*.*" ' Filter semua jenis file
+        openFileDialog.Title = "Select a File"
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            filePath = openFileDialog.FileName
+        End If
+
+        Label1.Text = filePath
+        ReadFile()
+    End Sub
+
+    Private Sub Clear()
+        ' Reset Label for filepath
+        Label1.Text = ""
+        ' Reset DataGridView
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Rows.Clear()
+        ' Reset PictureBox
+        PictureBox1.Image = Nothing
+    End Sub
+
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles Reset.Click
+        Clear()
+    End Sub
+
+    Private Sub DisplayJsonInDataGridView(jsonContent As String)
+        Dim jsonArray As JArray = JArray.Parse(jsonContent)
+        Dim dataTable As New DataTable()
+
+        ' Membuat kolom sesuai dengan properti JSON
+        For Each jsonObject As JObject In jsonArray
+            For Each prop As JProperty In jsonObject.Properties()
+                dataTable.Columns.Add(prop.Name)
+            Next
+            ' Keluar dari loop setelah iterasi pertama
+            Exit For
+        Next
+
+        ' Menambahkan baris ke DataTable
+        For Each jsonObject As JObject In jsonArray
+            Dim row As DataRow = dataTable.NewRow()
+            For Each prop As JProperty In jsonObject.Properties()
+                row(prop.Name) = prop.Value.ToString()
+            Next
+            dataTable.Rows.Add(row)
+        Next
+
+        ' Menetapkan DataTable sebagai DataSource untuk DataGridView
+        DataGridView1.DataSource = dataTable
+    End Sub
+
+    Private Sub ReadFile()
+        If Not String.IsNullOrEmpty(filePath) Then
+            Dim extension As String = Path.GetExtension(filePath)
+
+            Select Case extension.ToLower()
+                Case ".json"
+                    ' Memeriksa jika file adalah JSON
+                    Try
+                        Dim jsonContent As String = File.ReadAllText(filePath)
+                        DisplayJsonInDataGridView(jsonContent)
+                        ' MessageBox.Show("JSON Content: " & vbCrLf & jsonContent, "JSON File Content")
+                    Catch ex As Exception
+                        MessageBox.Show("Error reading JSON file: " & ex.Message)
+                    End Try
+
+                Case ".jpg", ".jpeg", ".png", ".gif"
+                    ' Memeriksa jika file adalah gambar
+                    Try
+                        ' Memuat gambar dari file ke PictureBox
+                        DisplayImageWithResize(filePath)
+                    Catch ex As Exception
+                        MessageBox.Show("Error displaying image: " & ex.Message)
+                    End Try
+
+                Case Else
+                    MessageBox.Show("Unsupported file type. Only JSON and image files are supported.")
+            End Select
+        Else
+            MessageBox.Show("Please select a file first.")
+        End If
+    End Sub
+
+    Private Sub DisplayImageWithResize(imagePath As String)
+        Try
+            ' Memuat gambar dari file
+            Dim originalImage As Image = Image.FromFile(imagePath)
+
+            ' Menghitung rasio aspek gambar
+            Dim aspectRatio As Double = CDbl(originalImage.Width) / CDbl(originalImage.Height)
+
+            ' Mengatur ukuran PictureBox
+            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+
+            ' Mengatur gambar dengan ukuran yang sesuai dengan PictureBox
+            If originalImage.Width > PictureBox1.Width Or originalImage.Height > PictureBox1.Height Then
+                ' Jika gambar lebih besar dari PictureBox, maka diperkecil agar muat
+                PictureBox1.Image = New Bitmap(originalImage, New Size(CInt(PictureBox1.Width), CInt(PictureBox1.Width / aspectRatio)))
+            Else
+                ' Jika gambar lebih kecil dari PictureBox, maka diatur sesuai ukuran aslinya
+                PictureBox1.Image = originalImage
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error displaying image: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Close_Click(sender As Object, e As EventArgs) Handles Close.Click
+        ' Panggil method Application.Exit() untuk menutup aplikasi
+        Application.Exit()
+    End Sub
+End Class
